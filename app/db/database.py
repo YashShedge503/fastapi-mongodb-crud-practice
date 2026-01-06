1a5b3d3c79e7d46327acabc99694bacc525daabf
Yash-Shedge-Factorial24 <yash.shedge@factorial24.com>
2026-01-06 23:15:38 +0530

    Refactor to production structure, env config, and add tests

--- app/db/database.py
+++ app/db/database.py
@@ -1,6 +1,11 @@
+import os
 from motor.motor_asyncio import AsyncIOMotorClient
+from dotenv import load_dotenv
 
-MONGO_URL = "mongodb://localhost:27017"
+load_dotenv()
+
+MONGO_URL = os.getenv("MONGO_URL")
+DATABASE_NAME = os.getenv("DATABASE_NAME")
 
 client = AsyncIOMotorClient(MONGO_URL)
 database = client.fastapi_crud_db_practice
